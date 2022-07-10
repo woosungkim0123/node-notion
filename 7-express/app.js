b7e3d34e0076850b8cb1a0c4a2f36ee3f1bec319
woosungkim0123 <sibu2005@naver.com>
2022-07-10 12:42:30 +0900

    before post study and clean code

--- 7-express/app.js
+++ 7-express/app.js
@@ -1,4 +1,4 @@
-import express, { application } from "express";
+import express from "express";
 const app = express();
 
 // all 과 use 차이
@@ -23,35 +23,14 @@ app.all("/api*", (req, res, next) => {
   next();
 });
 
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("first");
-    // send를 할때는 return 꼭 붙이기
-    // next("route"); 다음 미들웨어로 넘어가라 라고 설정할 수 있음
-    next(new Error("error")); // 처리하다가 예기치 못한 에러가 발생한다면
-  },
-  (req, res, next) => {
-    console.log("first2");
-    next();
-  }
-);
-
 app.get("/", (req, res) => {
-  res.setHeader("key", "value");
   res.status(201).send("created");
 });
 
 app.use((req, res, next) => {
-  res.status(404).send("Not avaliable!@_@"); // not found
+  res.status(404).send("Not avaliable!@_@");
 });
 
-// ip주소는 우리 서버가 네트워크상 어디있는지 알 수 있다면
-// 포트는 서버의 어떤 application에 접속하길 원하는지를 나타냄
-
-// 항상 어플리케이션 마지막에는 에러를 처리하는 것을 달아줘야함
-// 에러를 받는 에러 handler를 등록해줘야함
-// 미들웨어는 설정된 순서가 매우 중요함
 app.use((error, req, res, next) => {
   console.error(error);
   res.status(500).send("Sorry try later;");
