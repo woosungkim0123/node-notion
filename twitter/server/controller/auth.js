aa71f06cf607754abe86408ce18968dde6dfdbd9
woosungkim0123 <sibu2005@naver.com>
2022-07-16 20:39:58 +0900

    error check"

--- twitter/server/controller/auth.js
+++ twitter/server/controller/auth.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import {} from 'express-async-errors';
-import * as userRepository from '../data/auth.js';
-import { config } from '../config.js';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import {} from "express-async-errors";
+import * as userRepository from "../data/auth.js";
+import { config } from "../config.js";
 
 export async function signup(req, res) {
   const { username, password, name, email, url } = req.body;
@@ -27,11 +26,11 @@ export async function login(req, res) {
   const { username, password } = req.body;
   const user = await userRepository.findByUsername(username);
   if (!user) {
-    return res.status(401).json({ message: 'Invalid user or password' });
+    return res.status(401).json({ message: "Invalid user or password" });
   }
   const isValidPassword = await bcrypt.compare(password, user.password);
   if (!isValidPassword) {
-    return res.status(401).json({ message: 'Invalid user or password' });
+    return res.status(401).json({ message: "Invalid user or password" });
   }
   const token = createJwtToken(user.id);
   res.status(200).json({ token, username });
@@ -46,7 +45,7 @@ function createJwtToken(id) {
 export async function me(req, res, next) {
   const user = await userRepository.findById(req.userId);
   if (!user) {
-    return res.status(404).json({ message: 'User not found' });
+    return res.status(404).json({ message: "User not found" });
   }
   res.status(200).json({ token: req.token, username: user.username });
 }
