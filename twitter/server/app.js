6ac1d416dbd542f14f0cb3d5ec695a86a04a2c2b
woosungkim0123 <sibu2005@naver.com>
2022-07-16 20:38:22 +0900

    db_query

--- twitter/server/app.js
+++ twitter/server/app.js
@@ -1,22 +1,22 @@
-import express from "express";
-import "express-async-errors";
-import cors from "cors";
-import morgan from "morgan";
-import helmet from "helmet";
-import tweetsRouter from "./router/tweets.js";
-import authRouter from "./router/auth.js";
-import dotenv from "dotenv";
-dotenv.config();
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import tweetsRouter from './router/tweets.js';
+import authRouter from './router/auth.js';
+import { config } from './config.js';
+import { initSocket } from './connection/socket.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
-app.use("/tweets", tweetsRouter);
-app.use("/auth", authRouter);
+app.use('/tweets', tweetsRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
@@ -26,4 +26,6 @@ app.use((error, req, res, next) => {
   console.error(error);
   res.sendStatus(500);
 });
-app.listen(8080);
+
+const server = app.listen(config.host.port);
+initSocket(server);
--- twitter/server/controller/auth.js
+++ twitter/server/controller/auth.js
@@ -1,12 +1,8 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import {} from "express-async-errors";
-import * as userRepository from "../data/auth.js";
-
-// TODO: Make it secure!
-const jwtSecretKey = process.env.JWT_SECRET;
-const jwtExpiresInDays = process.env.JWT_EXPIRES_SEC;
-const bcryptSaltRounds = process.env.BCRYPT_SALT_ROUNDS;
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import {} from 'express-async-errors';
+import * as userRepository from '../data/auth.js';
+import { config } from '../config.js';
 
 export async function signup(req, res) {
   const { username, password, name, email, url } = req.body;
@@ -14,7 +10,7 @@ export async function signup(req, res) {
   if (found) {
     return res.status(409).json({ message: `${username} already exists` });
   }
-  const hashed = await bcrypt.hash(password, bcryptSaltRounds);
+  const hashed = await bcrypt.hash(password, config.bcrypt.saltRounds);
   const userId = await userRepository.createUser({
     username,
     password: hashed,
@@ -30,29 +26,26 @@ export async function login(req, res) {
   const { username, password } = req.body;
   const user = await userRepository.findByUsername(username);
   if (!user) {
-    return res.status(401).json({ message: "Invalid user or password" }); // 상세하게 보내주지않는 이유는 보안
+    return res.status(401).json({ message: 'Invalid user or password' });
   }
   const isValidPassword = await bcrypt.compare(password, user.password);
   if (!isValidPassword) {
-    return res.status(401).json({ message: "Invalid user or password" });
+    return res.status(401).json({ message: 'Invalid user or password' });
   }
   const token = createJwtToken(user.id);
-  // 사용자가 가입 후 다시 로그인을 하길 원한다면 -> login 시에만 token을 제공
-
-  // 가입하면 자동으로 로그인 하길 원한다면 -> singup시에 token을 제공
   res.status(200).json({ token, username });
 }
 
 function createJwtToken(id) {
-  return jwt.sign({ id }, jwtSecretKey, { expiresIn: jwtExpiresInDays });
+  return jwt.sign({ id }, config.jwt.secretKey, {
+    expiresIn: config.jwt.expiresInSec,
+  });
 }
 
 export async function me(req, res, next) {
-  // 미들웨어에서 체크했지만 다시 체크하는 이유
-  // 토큰에는 user_id만 넣으니까 해당 유저의 정보를 읽어오기위해
   const user = await userRepository.findById(req.userId);
   if (!user) {
-    return res.status(404).json({ message: "User not found" });
+    return res.status(404).json({ message: 'User not found' });
   }
   res.status(200).json({ token: req.token, username: user.username });
 }
