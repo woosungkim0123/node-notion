c136044d0b9f9bc8ca60fcd213f1188fb8447c30
woosungkim0123 <sibu2005@naver.com>
2022-07-23 15:53:58 +0900

    mongodb 연결 및 유저생성

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 function required(key, defaultValue = undefined) {
@@ -11,13 +11,16 @@ function required(key, defaultValue = undefined) {
 
 export const config = {
   jwt: {
-    secretKey: required('JWT_SECRET'),
-    expiresInSec: parseInt(required('JWT_EXPIRES_SEC', 86400)),
+    secretKey: required("JWT_SECRET"),
+    expiresInSec: parseInt(required("JWT_EXPIRES_SEC", 86400)),
   },
   bcrypt: {
-    saltRounds: parseInt(required('BCRYPT_SALT_ROUNDS', 12)),
+    saltRounds: parseInt(required("BCRYPT_SALT_ROUNDS", 12)),
   },
   host: {
-    port: parseInt(required('HOST_PORT', 8080)),
+    port: parseInt(required("HOST_PORT", 8080)),
+  },
+  db: {
+    host: required("DB_HOST"),
   },
 };
--- server/data/auth.js
+++ server/data/auth.js
@@ -1,17 +1,22 @@
 // abcd1234: $2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm
-let users = [
-  {
-    id: '1',
-    username: 'bob',
-    password: '$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm',
-    name: 'Bob',
-    email: 'bob@gmail.com',
-    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-  },
-];
 
+import { getUsers } from "../database/database.js";
+
+/*
+  어플리케이션 전반적으로  db를 사용하는 코드를 담고있는 대신에 데이터베이스와 직접 상호작용
+  하는 코드들을 data라는 패키지안에 auth와 tweet이라는 각각의 모듈로 서로 관련있는 database에 접근
+  할수 있도록 고립을 시켜뒀기 떄문에(캡슐화) 
+  db종류를 바꿀떄 어플리케이션코드는 전혀 손대지않고 한곳에서만 업데이트 함
+  이게 바로 유지보수성 높음, 확장이 높은 방식
+*/
 export async function findByUsername(username) {
-  return users.find((user) => user.username === username);
+  // 컬렉션을 가져오고
+  return getUsers()
+    .findOne({ username })
+    .then((data) => {
+      console.log(data);
+      return data;
+    });
 }
 
 export async function findById(id) {
@@ -19,7 +24,7 @@ export async function findById(id) {
 }
 
 export async function createUser(user) {
-  const created = { ...user, id: Date.now().toString() };
-  users.push(created);
-  return created.id;
+  return getUsers()
+    .insertOne(user)
+    .then((data) => data.insertedId.toString());
 }
