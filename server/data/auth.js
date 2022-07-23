3388329aae7310ea84c700e3ed2ed339af8fc255
woosungkim0123 <sibu2005@naver.com>
2022-07-23 16:16:37 +0900

    user join and login db

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,30 +1,25 @@
-// abcd1234: $2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm
-
 import { getUsers } from "../database/database.js";
+import MongoDb from "mongodb";
 
-/*
-  어플리케이션 전반적으로  db를 사용하는 코드를 담고있는 대신에 데이터베이스와 직접 상호작용
-  하는 코드들을 data라는 패키지안에 auth와 tweet이라는 각각의 모듈로 서로 관련있는 database에 접근
-  할수 있도록 고립을 시켜뒀기 떄문에(캡슐화) 
-  db종류를 바꿀떄 어플리케이션코드는 전혀 손대지않고 한곳에서만 업데이트 함
-  이게 바로 유지보수성 높음, 확장이 높은 방식
-*/
+const ObjectId = MongoDb.ObjectId;
+
 export async function findByUsername(username) {
-  // 컬렉션을 가져오고
-  return getUsers()
+  return getUsers() //
     .findOne({ username })
-    .then((data) => {
-      console.log(data);
-      return data;
-    });
+    .then(mapOptionalUser);
 }
 
 export async function findById(id) {
-  return users.find((user) => user.id === id);
+  return getUsers()
+    .findOne({ _id: new ObjectId(id) })
+    .then(mapOptionalUser);
 }
 
 export async function createUser(user) {
   return getUsers()
     .insertOne(user)
     .then((data) => data.insertedId.toString());
+}
+function mapOptionalUser(user) {
+  return user ? { ...user, id: user._id } : user;
 }
